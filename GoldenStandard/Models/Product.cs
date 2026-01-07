b8d7a766d66af79d7ec297f6c1249f3666f282e3
Your Name <you@example.com>
2026-01-08 01:42:28 +0400

    small fix product

--- GoldenStandard/Models/Product.cs
+++ GoldenStandard/Models/Product.cs
@@ -27,7 +27,7 @@ public class Product
             string fullText = comp + " " + desc;
 
             if (fullText.Contains("сахар")) score -= 15;
-            if (fullText.Contains("пальмовое масло")) score -= 20;
+            if (fullText.Contains("пальмовое масло")) score -= 25;
             if (fullText.Contains("гмо")) score -= 25;
             if (fullText.Contains("е-") || fullText.Contains("добавка е")) score -= 10;
             if (fullText.Contains("консервант")) score -= 5;
